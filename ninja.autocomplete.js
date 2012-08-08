538f7dc323dc346e469dff7baf89c57d46c80a36
Jamie Hoover ⚔ <dont.tase@me.com>
2012-08-08 11:31:05 -0700

    Initial Working Module
    Initial codebase, matching the 1.1.1 release of ninja .autocomplete to npm.

---
+++ ninja.autocomplete.js
@@ -0,0 +1,143 @@
+/*
+  Ninja Autocomplete $VERSION
+  http://ninjaui.com/autocomplete
+  Copyright 2008-$YEAR Jamie Hoover
+  Licensed per the terms of the Apache License v2.0
+  http://ninjaui.com/license.txt
+*/
+
+/*jshint
+  strict:true
+*/
+
+(function ($) {
+  'use strict';
+
+  $.Ninja.Autocomplete = function (element, options) {
+    this.$wrapper = $(element).wrap('<span class="nui-atc">').parent();
+    this.$list = $('<div>', {
+      css: {
+        top: this.$wrapper.height() - 2
+      }
+    });
+    this.datalist = [];
+    this.matchlist = [];
+    if (options && options.get) {
+      this.get = options.get;
+    }
+    this.index = -1;
+  };
+
+  $.Ninja.Autocomplete.prototype.last = function () {
+    return this.matchlist.length - 1;
+  };
+
+  $.Ninja.Autocomplete.prototype.list = function (datalist) {
+    var autocomplete = this;
+
+    if (!$.isFunction(autocomplete.get)) {
+      autocomplete.matchlist = $.map(datalist, function (option) {
+        var value = autocomplete.$element.val();
+
+        if (value !== option && new RegExp('^' + value, 'i').test(option)) {
+          return option;
+        } else {
+          return null;
+        }
+      });
+    } else {
+      autocomplete.matchlist = datalist;
+    }
+
+    autocomplete.$list.empty();
+
+    if (autocomplete.matchlist.length > 0) {
+      $.each(autocomplete.matchlist, function () {
+        var html = this;
+
+        $('<div>', {
+          html: html
+        }).on('hover.ninja', function () {
+          $(this).toggleClass('nui-hvr');
+        }).on('mousedown.ninja', function () {
+          autocomplete.$element.val(html);
+        }).appendTo(autocomplete.$list);
+      });
+
+      autocomplete.index = -1;
+
+      autocomplete.$list.appendTo(autocomplete.$wrapper);
+    }
+  };
+
+  $.Ninja.prototype.autocomplete = function (element, options) {
+    var autocomplete = $.extend(new $.Ninja.Component(element, options), new $.Ninja.Autocomplete(element, options));
+
+    if (options && options.datalist) {
+      autocomplete.datalist = options.datalist;
+    } else if (autocomplete.$element.data('list')) {
+      autocomplete.datalist = autocomplete.$element.data('list');
+    } else if (autocomplete.$element.attr('list')) {
+      $('datalist#' + autocomplete.$element.attr('list')).find('option').each(function () {
+        autocomplete.datalist.push($(this).val());
+      });
+    }
+
+    autocomplete.$element.attr({
+      autocomplete: 'off'
+    }).removeAttr('list');
+
+    autocomplete.$element.on('blur.ninja', function () {
+      autocomplete.$list.remove();
+    });
+
+    autocomplete.$element.on('keydown.ninja', function (event) {
+      var keycode = event.which;
+
+      if ($.ninja.key(keycode, ['escape', 'tab'])) {
+        autocomplete.$list.remove();
+      } else if (keycode === $.ninja.keys.enter && autocomplete.index > -1) {
+        autocomplete.$element.val(autocomplete.matchlist[autocomplete.index]);
+        autocomplete.$list.remove();
+      } else if ($.ninja.key(keycode, ['arrowDown', 'arrowUp'])) {
+        if (autocomplete.index > -1) {
+          autocomplete.$list.find(':eq(' + autocomplete.index + ')').removeClass('nui-hvr');
+        }
+
+        if (keycode === $.ninja.keys.arrowDown) {
+          if (autocomplete.index === autocomplete.last()) {
+            autocomplete.index = 0;
+          } else {
+            autocomplete.index += 1;
+          }
+        } else {
+          if (autocomplete.index <= 0) {
+            autocomplete.index = autocomplete.last();
+          } else {
+            autocomplete.index -= 1;
+          }
+        }
+
+        autocomplete.$list.find(':eq(' + autocomplete.index + ')').addClass('nui-hvr');
+      }
+    });
+
+    autocomplete.$element.on('focus.ninja, keyup.ninja', function (event) {
+      var keycode = event.which;
+
+      if (!autocomplete.$element.val()) {
+        autocomplete.$list.remove();
+      } else if (!$.ninja.key(keycode, ['arrowDown', 'arrowUp', 'escape', 'tab'])) {
+        if ($.isFunction(autocomplete.get)) {
+          autocomplete.get(autocomplete.$element.val(), function (datalist) {
+            autocomplete.datalist = datalist;
+
+            autocomplete.list(datalist);
+          });
+        } else {
+          autocomplete.list(autocomplete.datalist);
+        }
+      }
+    });
+  };
+}(window.jQuery));
---
+++ readme.md
@@ -0,0 +1,42 @@
+##[Ninja Autocomplete](http://ninjaui.com/autocomplete)
+*the [jQuery](http://jquery.com/ "jQuery 1.7") plugin for lethal interaction*
+
+###Develop
+
+1.) [`Fork`](fork_select) Ninja Autocomplete.
+
+2.) Clone from your fork.
+
+    git clone git@github.com:$GITHUB_USER/ninja.autocomplete.git ~/Development
+
+3.) Install dependencies:
+
+    cd ~/Development/ninja.autocomplete
+    npm install
+
+Build and test latest development version:
+
+    make
+
+**Automatically** watch and rebuild/retest when source/tests change:
+
+    make watch
+
+###Authors
+[`uipoet`](/uipoet "Jamie Hoover") and [`faisal`](/faisal "Faisal N. Jawdat")
+
+###License
+Copyright 2008-2012 Jamie Hoover.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+
+[ninjaui.com/license.txt](http://ninjaui.com/license.txt)
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
